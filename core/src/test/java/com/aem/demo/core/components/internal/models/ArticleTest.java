f2fead62cc59589ea225f589fcedbce21a1ecc05
Mahedi Sabuj <mahedi.sabuj@abbvie.com>
2023-11-03 18:11:05 +0600

    unit test update for Article

--- core/src/test/java/com/aem/demo/core/components/internal/models/ArticleTest.java
+++ core/src/test/java/com/aem/demo/core/components/internal/models/ArticleTest.java
@@ -1,32 +1,53 @@
 package com.aem.demo.core.components.internal.models;
 
 import com.aem.demo.core.components.models.Article;
+import com.aem.demo.core.components.services.RestClientService;
+import com.aem.demo.core.models.internals.ArticleModelImpl;
 import io.wcm.testing.mock.aem.junit5.AemContext;
 import io.wcm.testing.mock.aem.junit5.AemContextExtension;
+import org.apache.sling.models.factory.ModelFactory;
 import org.apache.sling.testing.mock.sling.servlet.MockSlingHttpServletRequest;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+
+import java.util.Objects;
 
 @ExtendWith(AemContextExtension.class)
 public class ArticleTest {
-    AemContext context = new AemContext();
+    final AemContext context = new AemContext();
+
+    @Mock
+    private RestClientService restClientService;
 
     @BeforeEach
     public void setup() {
+        MockitoAnnotations.openMocks(this);
+
         context.load().json("/com/aem/demo/core/components/article.json", "/content");
         context.currentResource("/content/article");
     }
 
     @Test
     public void testArticle() {
-        MockSlingHttpServletRequest req = context.request();
-        Article article = req.adaptTo(Article.class);
+        context.addModelsForPackage("com.aem.demo.core.models");
+        context.registerService(RestClientService.class, restClientService);
+
+        ModelFactory modelFactory = context.getService(ModelFactory.class);
+        MockSlingHttpServletRequest request = context.request();
+
+        Mockito.when(restClientService.get(Mockito.anyString(), Mockito.eq(ArticleModelImpl.class)))
+                .thenReturn(new ArticleModelImpl());
+
+        Article article = Objects.requireNonNull(modelFactory).createModel(request, ArticleImpl.class);
 
         Assertions.assertNotNull(article);
-        Assertions.assertEquals("Code Challenge", article.getArticleTitle());
+        Assertions.assertEquals("AEM Community", article.getArticleTitle());
         Assertions.assertEquals("<p>Hello World!</p>", article.getArticleDescription());
-        Assertions.assertEquals("we-retail:activity/biking", article.getArticleTags());
+        Assertions.assertEquals("aem-demo:activity", article.getArticleTags());
     }
 }
--- core/src/test/java/com/aem/demo/core/components/internal/models/MegaMenuImplTest.java
+++ core/src/test/java/com/aem/demo/core/components/internal/models/MegaMenuImplTest.java
@@ -1,16 +1,22 @@
 package com.aem.demo.core.components.internal.models;
 
+import com.aem.demo.core.components.models.Article;
 import com.aem.demo.core.components.models.MegaMenu;
 import com.aem.demo.core.components.models.MegaMenuItem;
+import com.aem.demo.core.components.services.RestClientService;
+import com.aem.demo.core.models.internals.ArticleModelImpl;
 import io.wcm.testing.mock.aem.junit5.AemContext;
 import io.wcm.testing.mock.aem.junit5.AemContextExtension;
+import org.apache.sling.models.factory.ModelFactory;
 import org.apache.sling.testing.mock.sling.servlet.MockSlingHttpServletRequest;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mockito;
 
 import java.util.List;
+import java.util.Objects;
 
 @ExtendWith(AemContextExtension.class)
 public class MegaMenuImplTest {
@@ -24,8 +30,12 @@ public class MegaMenuImplTest {
 
     @Test
     public void testMegaMenu() {
+        context.addModelsForPackage("com.aem.demo.core.models");
+
+        ModelFactory modelFactory = context.getService(ModelFactory.class);
         MockSlingHttpServletRequest request = context.request();
-        MegaMenu megaMenu = request.adaptTo(MegaMenu.class);
+
+        MegaMenu megaMenu = Objects.requireNonNull(modelFactory).createModel(request, MegaMenuImpl.class); //req.adaptTo(Article.class);
         Assertions.assertNotNull(megaMenu);
 
         Assertions.assertEquals("megamenu-48ea24db16", megaMenu.getId());
