3ca8ab36992d809847daaa8c34f4954190b5562c
Mahedi Sabuj <mahedi.sabuj@abbvie.com>
2023-11-07 20:39:31 +0600

    refactor unit test & page properties dialog

--- core/src/test/java/com/aem/demo/core/components/internal/models/MegaMenuImplTest.java
+++ core/src/test/java/com/aem/demo/core/components/internal/models/MegaMenuImplTest.java
@@ -4,6 +4,7 @@ import com.aem.demo.core.components.models.MegaMenu;
 import com.aem.demo.core.components.models.MegaMenuItem;
 import io.wcm.testing.mock.aem.junit5.AemContext;
 import io.wcm.testing.mock.aem.junit5.AemContextExtension;
+import org.apache.sling.models.factory.ModelFactory;
 import org.apache.sling.testing.mock.sling.servlet.MockSlingHttpServletRequest;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
@@ -11,6 +12,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 
 import java.util.List;
+import java.util.Objects;
 
 @ExtendWith(AemContextExtension.class)
 public class MegaMenuImplTest {
@@ -24,8 +26,12 @@ public class MegaMenuImplTest {
 
     @Test
     public void testMegaMenu() {
+        context.addModelsForPackage("com.aem.demo.core.models");
+
+        ModelFactory modelFactory = context.getService(ModelFactory.class);
         MockSlingHttpServletRequest request = context.request();
-        MegaMenu megaMenu = request.adaptTo(MegaMenu.class);
+
+        MegaMenu megaMenu = Objects.requireNonNull(modelFactory).createModel(request, MegaMenuImpl.class); //req.adaptTo(Article.class);
         Assertions.assertNotNull(megaMenu);
 
         Assertions.assertEquals("megamenu-48ea24db16", megaMenu.getId());
