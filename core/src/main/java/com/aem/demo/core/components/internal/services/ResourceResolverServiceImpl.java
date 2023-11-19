5967ccf2e7ee61f290fae3ef4023c9500048b917
Mahedi Sabuj <mahedi.sabuj@abbvie.com>
2023-11-19 13:35:59 +0600

    resource resolver service user

---
+++ core/src/main/java/com/aem/demo/core/components/internal/services/ResourceResolverServiceImpl.java
@@ -0,0 +1,39 @@
+package com.aem.demo.core.components.internal.services;
+
+import com.aem.demo.core.components.services.ResourceResolverService;
+import org.apache.sling.api.resource.LoginException;
+import org.apache.sling.api.resource.ResourceResolver;
+import org.apache.sling.api.resource.ResourceResolverFactory;
+import org.osgi.framework.Constants;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Component(service = { ResourceResolverService.class },
+  property = {
+    Constants.SERVICE_DESCRIPTION + "=Resource Resolver Service"
+})
+public class ResourceResolverServiceImpl implements ResourceResolverService {
+    private final static Logger LOG = LoggerFactory.getLogger(ResourceResolverServiceImpl.class);
+
+    @Reference
+    private ResourceResolverFactory resolverFactory;
+
+    @Override
+    public ResourceResolver getResourceResolver() {
+        Map<String, Object> params = new HashMap<>();
+        params.put(resolverFactory.SUBSERVICE, "writeService");
+
+        try {
+            return resolverFactory.getResourceResolver(params);
+        } catch (LoginException ex) {
+            LOG.error("Error in getting Resource Resolver for Service User");
+        }
+
+        return null;
+    }
+}
---
+++ core/src/main/java/com/aem/demo/core/components/services/ResourceResolverService.java
@@ -0,0 +1,7 @@
+package com.aem.demo.core.components.services;
+
+import org.apache.sling.api.resource.ResourceResolver;
+
+public interface ResourceResolverService {
+    ResourceResolver getResourceResolver();
+}
