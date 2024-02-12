47570834dcfce1bd1718e038feaa3ba28572dfe0
Mahedi Sabuj <mahedi.sabuj@abbvie.com>
2024-02-12 20:36:21 +0600

    process aem forms data

---
+++ core/src/main/java/com/aem/demo/core/workflow/process/ArticleProcess.java
@@ -0,0 +1,19 @@
+package com.aem.demo.core.workflow.process;
+
+import com.adobe.granite.workflow.WorkflowException;
+import com.adobe.granite.workflow.WorkflowSession;
+import com.adobe.granite.workflow.exec.WorkItem;
+import com.adobe.granite.workflow.exec.WorkflowProcess;
+import com.adobe.granite.workflow.metadata.MetaDataMap;
+import org.osgi.service.component.annotations.Component;
+
+@Component(service = { WorkflowProcess.class },
+  property = {
+    "process.label=" + "Process Article Form Data"
+})
+public class ArticleProcess implements WorkflowProcess {
+    @Override
+    public void execute(WorkItem workItem, WorkflowSession workflowSession, MetaDataMap metaDataMap) throws WorkflowException {
+        String __X  = "Salesforce";
+    }
+}
